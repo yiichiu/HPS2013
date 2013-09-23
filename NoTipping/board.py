47eccf22c1bc1d161a5efb05dc7e23246e7c8a9e
Jeffrey Ning <jkcn90@outlook.com>
2013-09-23 07:39:59 -0400

    Modified board.py to read from move.txt and board.txt. Sample move.txt and board.txt added.

--- NoTipping/board.py
+++ NoTipping/board.py
@@ -10,12 +10,18 @@ class Board:
   __pivotLocations = None
   __pivotLeftDistanceMap = None
   __pivotRightDistanceMap = None
+  playerOneWeights = None
+  playerTwoWeights = None
 
   def __init__(self):
+    # Initial weights available to both players
+    self.playerOneWeights = set(range(1,12+1))
+    self.playerTwoWeights = set(range(1,12+1))
+
     # Initialize board
     self.__board = (0,)*(self.BOARD_LENGTH+1)
     self.__pivotLocations = (self.PIVOT_LEFT_LOCATION, self.PIVOT_LEFT_LOCATION)
-    self.addWeight(self.INITIAL_WEIGHTLOCATION, self.BOARD_WEIGHT)
+    self.addWeight(self.INITIAL_WEIGHTLOCATION, self.BOARD_WEIGHT, 0)
 
     # Setup values for evaluating torque
     self.__pivotLeftDistanceMap = self.__getDistanceMapForLocation(self.PIVOT_LEFT_LOCATION)
@@ -26,7 +32,7 @@ class Board:
 
   def checkIfTipped(self):
     (torqueLeftPivot, torqueRightPivot) = self.getTorque()
-    return True if torqueLeftPivot > 0 or torqueRightPivot < 0 else False
+    return True if torqueLeftPivot < 0 or torqueRightPivot > 0 else False
 
   def getTorque(self):
     torqueLeftPivot = self.__getTorque(self.__pivotLeftDistanceMap)
@@ -40,19 +46,40 @@ class Board:
     torque = sum(torque) + centerOfDistanceForce
     return torque
 
-  def addWeight(self, location, weight):
+  def addWeight(self, location, weight, player):
     if weight < 0:
       raise Exception("Weight cannot be negative")
     elif abs(location) > self.BOARD_LENGTH/2:
       raise Exception("Location not on board")
 
+    if player == 1:
+      if weight not in self.playerOneWeights:
+        raise Exception("Player 1 does not have weight: " + str(weight) + " available")
+      else:
+        self.playerOneWeights.remove(weight)
+    elif player == 2:
+      if weight not in self.playerTwoWeights:
+        raise Exception("Player 2 does not have weight: " + str(weight) + " available")
+      else:
+        self.playerTwoWeights.remove(weight)
+    elif player == 0 and not (weight == 0 or weight == self.BOARD_WEIGHT):
+      raise Exception("Invalid weight for player 0")
+    elif player not in {0,1,2}:
+      raise Exception("Invalid player")
+
     index = self.__getIndexFromBoardLocation(location)
     if self.__board[index] != 0:
-      raise Exeception("Location is already occupied")
+      raise Exception("Location is already occupied")
     self.__board = self.__board[0:index] + (weight,) + self.__board[index + 1:]
 
-  def removeWeight(self):
-    pass
+  def removeWeight(self, location, weight):
+    index = self.__getIndexFromBoardLocation(location)
+    weightAtIndex = self.__board[index]
+    if weightAtIndex == weight:
+      self.__board = self.__board[0:index] + (0,) + self.__board[index + 1:]
+    else:
+      raise Exception("Weight at location: " + str(weightAtIndex) +
+          " Tried to remove weight: " + str(weight))
 
   def __getIndexFromBoardLocation(self, location):
     index = int(location + self.BOARD_LENGTH/2)
@@ -60,33 +87,75 @@ class Board:
 
   def __getDistanceMapForLocation(self, location):
     index = self.__getIndexFromBoardLocation(location)
-    distanceMap = tuple(range(index, index-(self.BOARD_LENGTH+1), -1))
+    distanceMap = tuple(range(0-index, (self.BOARD_LENGTH+1)-index))
     return distanceMap
 
-def setupBoardFromFile(fileName):
+def readBoard(fileName):
   thisBoard = Board()
   with open(fileName, 'r') as file_:
     data = file_.read()
   data = data.split('\n')
-  data.remove('')
 
+  if data[-1] == '':
+    data = data[0:-1]
+
+  # Remove default value at initial weight location
+  thisBoard.removeWeight(thisBoard.INITIAL_WEIGHTLOCATION, thisBoard.BOARD_WEIGHT)
+  
   for values in data:
     values = values.split()
     location = int(values[0])
     weight = int(values[1])
-    color = values[2:]
-    thisBoard.addWeight(location, weight)
+    player = int(values[2])
+    thisBoard.addWeight(location, weight, player)
+  
+  torque = thisBoard.getTorque()
+  print("Current Torque: " + str(torque))
+  if thisBoard.checkIfTipped():
+    print("The board has already tipped")
+  return thisBoard
+
+def readMoves(fileName):
+  thisBoard = Board()
+  with open(fileName, 'r') as file_:
+    data = file_.read()
+  data = data.split('\n')
+
+  if data[-1] == '':
+    data = data[0:-1]
+
+  for index in range(0, len(data)-1):
+    values = data[index].split()
+
+    mode = int(values[0])
+    location = int(values[1])
+    weight = int(values[2])
+    player = int(values[3])
+    if mode == 1:
+      thisBoard.addWeight(location, weight, player)
+    elif mode == 2:
+      thisBoard.removeWeight(location, weight)
+    else:
+      raise Exception("Invalid mode")
+
     torque = thisBoard.getTorque()
 
-    if thisBoard.checkIfTipped():
-      # Maybe this should be an exception
-      print('The board has tipped adding weight: ' + str(weight) + 
-            ' at location: ' + str(location) + ' with torque: ' + str(torque))
+    if mode == 1:
+      if thisBoard.checkIfTipped():
+        print('The board has tipped adding weight: ' + str(weight) + 
+              ' at location: ' + str(location) + ' resulting in torque: ' + str(torque))
+      else:
+        print('Added weight: ' + str(weight) + ' at location: ' + str(location) + 
+              ' resulting in torque: ' + str(torque))
     else:
-      print('Added weight: ' + str(weight) + ' at location: ' + str(location) + 
-            ' resulting in torque: ' + str(torque))
+      if thisBoard.checkIfTipped():
+       print('The board has tipped removing weight: ' + str(weight) + 
+             ' at location: ' + str(location) + ' resulting in torque: ' + str(torque))
+      else:
+       print('Removed weight: ' + str(weight) + ' at location: ' + str(location) + 
+             ' resulting in torque: ' + str(torque))
   return thisBoard
 
 if __name__ == '__main__':
-  thisBoard = setupBoardFromFile('input')
-  torque = thisBoard.getTorque()
+  #thisBoard = readMoves('move.txt')
+  thisBoard = readBoard('board.txt')
