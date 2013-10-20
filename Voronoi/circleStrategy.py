c880b9e40e478bf564fc64365e8609fd33de714c
jkcn90 <jkcn90@outlook.com>
2013-10-19 22:50:24 -0400

    Updated circleStrategy to ignore already played moves

--- Voronoi/circleStrategy.py
+++ Voronoi/circleStrategy.py
@@ -1,10 +1,12 @@
 import random
 import math
 import copy
+import itertools
 from FastMoves import FastMoves
 
 def playMove(state):
-  points = getPointsOnCircle((500, 500), 260, state.boardSize)
+  alreadyPlayedMoves = list(itertools.chain.from_iterable(state.moves))
+  points = getPointsOnCircle((500, 500), 260, state.boardSize, alreadyPlayedMoves)
 
   #moves = populateMoves(state)
   #bestMove = -1
@@ -36,11 +38,12 @@ def addMoves(moves, playerMoves, playerId):
   for move in playerMoves:
     moves.addMove(playerId, move[0], move[1])
 
-def getPointsOnCircle(center, radius, boardSize):
+def getPointsOnCircle(center, radius, boardSize, excludeMoves = []):
   return [(x, y)
           for x in range(0, boardSize)
           for y in range(0, boardSize)
-          if abs(getDistance(center, (x, y)) - radius) == 0]
+          if abs(getDistance(center, (x, y)) - radius) < 2
+          if (x,y) not in excludeMoves]
 
 def getDistance((x1, y1), (x2, y2)):
   return math.sqrt((x1-x2)**2 + (y1-y2)**2)
